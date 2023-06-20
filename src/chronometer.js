fd0c42abc0cbc8a9238e5e2123e078aa803ee81d
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2023-06-20 21:24:58 +0200

    chronometer updated

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,60 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    if (printTimeCallback !== undefined) {
+      this.intervalId = setInterval(() => {
+        this.currentTime++;
+        printTimeCallback();
+      }, 1000);
+    } else {
+      this.intervalId = setInterval(() => {
+        this.currentTime++;
+      }, 1000);
+    }
   }
 
   getMinutes() {
-    // ... your code goes here
+    const totalMins = Math.floor(this.currentTime / 60);
+    return totalMins;
   }
 
   getSeconds() {
-    // ... your code goes here
+    const afterCurrentTimeSecs = this.currentTime % 60;
+    return afterCurrentTimeSecs;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    const stringValue = "0" + value.toString();
+    return stringValue.slice(-2);
+    // let stringValue = `${value}`;
+    // if (stringValue.length < 2) {
+    //   stringValue = `0${stringValue}`;
+    // }
+
+    // return stringValue;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    const mins = this.computeTwoDigitNumber(this.getMinutes());
+    const secs = this.computeTwoDigitNumber(this.getSeconds());
+    return `${mins}:${secs}`;
   }
+
+  //bonus
+  //getCentiseconds()
+  //split(){
+
+  //}
 }
